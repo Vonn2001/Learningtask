f6f62051f88e0a858bd54564d510ee86167afb4a
Vonn2001 <100071064+Vonn2001@users.noreply.github.com>
2022-09-04 21:45:11 +0800

    Add files via upload

---
+++ Luu.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="hang.css">
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<script src=".js"></script>
+<title> Apology </title>
+</head>
+<body>
+<h1> Hellooooo Brooo! I just wanna say I'm sorry</h1>
+<br>
+<h1> Can You Forgive Me? </h1>
+<div class="container">
+        <input type="radio" name="radio" id="opt1">
+        <label for="opt1" class="label1">
+            <span target="_blank" onclick="myFunction1()">YES</span>
+        </label>
+        <input type="radio" name="radio" id="opt2">
+        <label for="opt2" class="label2">
+            <span>NO</span>
+        </label>
+    </div>
+
+</body>
+</html>
---
+++ hang.js
@@ -0,0 +1,17 @@
+alert("Are You Hang??? If no then go away!");
+
+function myFunction1() {
+  var txt;
+  if (confirm("Really?")) {
+    window.location.href = "yey.html"
+  } else {
+    alert("Sorry That button is sadly broken bro, try again :)");
+  }
+  document.getElementById("demo").innerHTML = txt;
+  }
+  
+  function myFunction2() {{
+  alert("Sorry That button is broken bro :)");
+  }
+  document.getElementById("demo").innerHTML = txt;
+  }
